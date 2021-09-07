828f85767cd5e1ac97e03b57f5d49ec15eabf2af
heidegomes <heidegc@hotmail.com>
2021-09-07 18:44:37 -0300

    utilizando JS Externo

---
+++ utilizandoJSExterno/diferencaDeIdade.js
@@ -0,0 +1,12 @@
+let nomeVelha = prompt("Informe o nome da pessoa mais velha: ");
+let idadeVelha = prompt("Informe a idade da pessoa mais velha: ");
+let nomeNova = prompt("Informe o nome da pessoa mais nova: ");
+let idadeNova = prompt("Informe a idade da pessoa mais nova: ");
+
+let diferencaIdade = idadeVelha - idadeNova;
+console.log("A diferença de idade entre essas pessoas é: " + diferencaIdade);
+
+alert( "Nome: " + nomeVelha + "\nIdade: " + idadeVelha + 
+"\n\nNome: " + nomeNova + "\nIdade: " + idadeNova + 
+"\n\nA diferença de idade é : " + diferencaIdade
+);
