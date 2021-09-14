0984b9018f48d7905c823b83d74405c5be1dbfe0
heidegomes <heidegc@hotmail.com>
2021-09-13 21:23:20 -0300

    Invertendo um nome

---
+++ revisandoAsRepeticoes/revisandoAsRepeticoes.js
@@ -0,0 +1,13 @@
+let spaceshipName = prompt("Digite o nome da nave: ");
+
+let invertedName = "";
+
+
+for(let i = spaceshipName.length - 1; i >= 0 ; i--){
+    if(spaceshipName[i] == "e"){
+        break;
+    }
+    invertedName += spaceshipName[i];
+}
+alert("Nome original da nave: " + spaceshipName + "\nNome após ocultação: " + invertedName);
+
