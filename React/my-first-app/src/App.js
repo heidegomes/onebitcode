9e0fb83d1c3b7c71b0b18940b99187e8254178fa
heidegomes <heidegc@hotmail.com>
2021-11-14 18:21:48 -0300

    Gerando meu primeiro projeto react

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './Hello.js';
+
+
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/my-first-app/src/Hello.js
@@ -0,0 +1,21 @@
+ import React from "react";
+
+//component de função
+//  function HelloWorld() {
+//     return <h1>Hello World</h1>;
+//   }
+
+// component de arrow function
+//   const HelloWorld = () => {
+//     return <h1>Hello World</h1>;
+//   }
+
+//component de classe
+class HelloWorld extends React.Component {
+    render (){
+        return <h1> Hello World </h1>;
+    }
+
+}
+
+  export default HelloWorld;
