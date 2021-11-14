f99eb571c64e9ecea30ee3efbbbf69fdf0b5a654
heidegomes <heidegc@hotmail.com>
2021-11-14 18:27:15 -0300

    Criando um component

---
+++ React/criando-um-component/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HelloWorld from './Hello';
+import LoremIpsum from './components/loremipsum';
+
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+      <LoremIpsum/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/criando-um-component/src/components/loremipsum.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoremIpsum = () => {
+    return <p>Eu espero que você aprenda muitooo react o/</p>
+}
+
+export default LoremIpsum
