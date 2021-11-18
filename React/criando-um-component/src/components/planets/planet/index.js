2cea5f0c23fe97b0279d40e03767dbb023203c87
heidegomes <heidegc@hotmail.com>
2021-11-17 22:21:22 -0300

    Renderização Condicional

--- React/criando-um-component/src/components/planets/planet/index.js
+++ React/criando-um-component/src/components/planets/planet/index.js
@@ -1,13 +1,19 @@
 import React, {Fragment} from 'react';
 import GrayImg from '../../shared/gray_img'
 import DescriptionWithLink from '../../shared/description_with_link';
+import { getByTitle } from '@testing-library/react';
 
 const Planet = (props) => {
+    let title;
+    if(props.title_with_underline)
+        title = <h4><u>{props.name}</u></h4>
+    else
+        title = <h4>{props.name}</h4>
     return (
-        <div onClick={() => props.clickOnPlanet(props.name)}>
-            <h4>{props.name}</h4>
+        <div>
+            {title}
             <DescriptionWithLink description={props.description} link={props.link}/>
-            <GrayImg img_url={props.img_url}/>
+            <GrayImg img_url={props.img_url} gray={props.gray}/>
         </div>
     )
 }
