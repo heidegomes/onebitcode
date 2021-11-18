fa1f0eef329e6ce0e9ee6ec3c5411dda73c2d3d5
heidegomes <heidegc@hotmail.com>
2021-11-18 20:05:24 -0300

    Renderizando multiplos components

--- React/criando-um-component/src/components/planets/planet/index.js
+++ React/criando-um-component/src/components/planets/planet/index.js
@@ -1,9 +1,14 @@
 import React, {Fragment} from 'react';
 import GrayImg from '../../shared/gray_img'
 import DescriptionWithLink from '../../shared/description_with_link';
-import { getByTitle } from '@testing-library/react';
+
 
 const Planet = (props) => {
+    const names = ['a', 'b', 'c', 'd']
+    const satellites = names.map((n) => 
+        <li>Satélite {n}</li>
+    )
+
     let title;
     if(props.title_with_underline)
         title = <h4><u>{props.name}</u></h4>
@@ -14,6 +19,12 @@ const Planet = (props) => {
             {title}
             <DescriptionWithLink description={props.description} link={props.link}/>
             <GrayImg img_url={props.img_url} gray={props.gray}/>
+
+            <h4>Satélites</h4>
+            <ul>
+                {satellites}
+            </ul>
+            <hr/>
         </div>
     )
 }
