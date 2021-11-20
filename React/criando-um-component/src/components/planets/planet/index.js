ad5c106b530b28dfb2efa6c8d224dcc51dbf86ef
heidegomes <heidegc@hotmail.com>
2021-11-20 11:18:21 -0300

    Renderizando multiplos elementos no JSX

--- React/criando-um-component/src/components/planets/planet/index.js
+++ React/criando-um-component/src/components/planets/planet/index.js
@@ -1,30 +1,30 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import GrayImg from '../../shared/gray_img'
 import DescriptionWithLink from '../../shared/description_with_link';
 
 
 const Planet = (props) => {
     const names = ['a', 'b', 'c', 'd']
-    const satellites = names.map((n) => 
-        <li>Satélite {n}</li>
-    )
+
 
     let title;
-    if(props.title_with_underline)
+    if (props.title_with_underline)
         title = <h4><u>{props.name}</u></h4>
     else
         title = <h4>{props.name}</h4>
     return (
         <div>
             {title}
-            <DescriptionWithLink description={props.description} link={props.link}/>
-            <GrayImg img_url={props.img_url} gray={props.gray}/>
+            <DescriptionWithLink description={props.description} link={props.link} />
+            <GrayImg img_url={props.img_url} gray={props.gray} />
 
             <h4>Satélites</h4>
             <ul>
-                {satellites}
+                {names.map((n) =>
+                    <li>Satélite {n}</li>
+                )}
             </ul>
-            <hr/>
+            <hr />
         </div>
     )
 }
