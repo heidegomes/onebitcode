29921e56b2092911b89116fe145dcad19536276f
heidegomes <heidegc@hotmail.com>
2021-11-24 21:25:16 -0300

    Usando State e Lifecycle

--- React/criando-um-component/src/components/planets/index.js
+++ React/criando-um-component/src/components/planets/index.js
@@ -1,32 +1,27 @@
 import React, { Fragment } from 'react';
 import Planet from './planet';
 
+async function getPlanets(){
+    let response = await fetch('http://localhost:3009/api/planets.json')
+    let data = await response.json()
+    return data;
+}
+
 class Planets extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             mostrar: true,
-            planets: [
-                {
-                    name: "Mercúrio",
-                    description: "Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. ",
-                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
-                    link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-                },
-                {
-                    name: "Plutão",
-                    description: "Plutão, formalmente designado 134340 Plutão, é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[12] uma região de corpos além da órbita de Netuno.",
-                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
-                    link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o" 
-                },
-                {
-                    name: "Vênus",
-                    description: "Vénus (português europeu) ou Vênus (português brasileiro) (AO 1990: Vénus ou Vênus)[1] é o segundo planeta do Sistema Solar em ordem de distância a partir do Sol, orbitando-o a cada 224,7 dias. Recebeu seu nome em homenagem à deusa romana do amor e da beleza Vénus, equivalente a Afrodite. ",
-                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped2%29.jpg/280px-PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped2%29.jpg",
-                    link: "https://pt.wikipedia.org/wiki/V%C3%A9nus_(planeta)" 
-                }
-            ]
+            planets: [ ]
         }
+    }
+
+    componentDidMount () {
+        getPlanets().then(data => {
+            this.setState(state => ({
+                planets: data['planets']
+            }))
+        })
     }
 
     removeLast = () => {
@@ -52,6 +47,7 @@ class Planets extends React.Component {
                 <hr />
                 {this.state.planets.map((planet) =>
                     <Planet 
+                    id={planet.id}
                     name={planet.name}
                     description={planet.description}
                     img_url={planet.img_url}
--- React/criando-um-component/src/components/planets/planet/index.js
+++ React/criando-um-component/src/components/planets/planet/index.js
@@ -2,31 +2,51 @@ import React, { Fragment } from 'react';
 import GrayImg from '../../shared/gray_img'
 import DescriptionWithLink from '../../shared/description_with_link';
 
-
-const Planet = (props) => {
-    const names = ['a', 'b', 'c', 'd', 'f', 'g']
-
-
-    let title;
-    if (props.title_with_underline)
-        title = <h4><u>{props.name}</u></h4>
-    else
-        title = <h4>{props.name}</h4>
-    return (
-        <div>
-            {title}
-            <DescriptionWithLink description={props.description} link={props.link} />
-            <GrayImg img_url={props.img_url} gray={props.gray} />
-
-            <h4>Satélites</h4>
-            <ul>
-                {names.map((n) =>
-                    <li>Satélite {n}</li>
-                )}
-            </ul>
-            <hr />
-        </div>
-    )
+async function getSattellites(id){
+    let response = await fetch(`http://localhost:3009/api/${id}.json`)
+    let data = await response.json();
+    return data;
 }
+
+class Planet extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            satellites: []
+        }
+    }
+
+    componentDidMount(){
+        getSattellites(this.props.id).then(data => {
+            this.setState(state => ({
+                satellites: data['satellites']
+            }))
+        })
+    }
+
+    render() {
+        let title;
+        if (this.props.title_with_underline)
+            title = <h4><u>{this.props.name}</u></h4>
+        else
+            title = <h4>{this.props.name}</h4>
+
+        return (
+            <div>
+                {title}
+                <DescriptionWithLink description={this.props.description} link={this.props.link} />
+                <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
+                <h4>Satélites</h4>
+                <ul>
+                    {this.state.satellites.map((satellite, index) =>
+                        <li key={index}>{satellite.name}</li>
+                    )}
+                </ul>
+                <hr/>
+            </div>
+        )
+    }
+}
+
 
 export default Planet;
